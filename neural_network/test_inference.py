c7fd69404b3bee4070b4194cfa33be627c474055
Fernando Castillo <fcastilho999@gmail.com>
2025-04-11 09:51:47 -0300

    test: update test_inference.py

--- neural_network/test_inference.py
+++ neural_network/test_inference.py
@@ -30,17 +30,27 @@ for i in range(len(test_dataset)):
     true_field = true_field.numpy()
     error_field = np.abs(true_field - pred_field)
 
-    # Um gráfico 3-em-1 para cada variável
     for j in range(3):
-        fig, axs = plt.subplots(1, 3, figsize=(15, 4))
-        axs[0].imshow(true_field[:, :, j], cmap='jet')
+        fig, axs = plt.subplots(1, 3, figsize=(16, 4))
+        
+        # Campo real
+        im0 = axs[0].imshow(true_field[:, :, j], cmap='jet')
         axs[0].set_title(f'{var_names[j]} real | Re = {Re_val:.2f}')
-        axs[1].imshow(pred_field[:, :, j], cmap='jet')
+        plt.colorbar(im0, ax=axs[0], fraction=0.046, pad=0.04)
+
+        # Campo predito
+        im1 = axs[1].imshow(pred_field[:, :, j], cmap='jet')
         axs[1].set_title(f'{var_names[j]} predito')
-        axs[2].imshow(error_field[:, :, j], cmap='jet')
+        plt.colorbar(im1, ax=axs[1], fraction=0.046, pad=0.04)
+
+        # Erro absoluto
+        im2 = axs[2].imshow(error_field[:, :, j], cmap='jet')
         axs[2].set_title(f'Erro absoluto')
+        plt.colorbar(im2, ax=axs[2], fraction=0.046, pad=0.04)
+
         for ax in axs:
             ax.axis('off')
+
         plt.tight_layout()
         plt.savefig(f'comparacoes_com_erro/{var_names[j]}_Re_{Re_val:.2f}.png')
         plt.close()
